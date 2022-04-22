2c9b70839bf8b28a9b00c11cfe97e3eec86658ed
tysprouse <tsprouse@ucsd.edu>
2022-04-21 17:51:52 -0700

    fixed test-file.md and added spaces to MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,14 +11,19 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
+        
         while(currentIndex < markdown.length()) {
+            //System.out.println(currentIndex);
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
+
             toReturn.add(markdown.substring(openParen + 1, closeParen));
+
             currentIndex = closeParen + 1;
         }
+        
 
         return toReturn;
     }
--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,5 +1,10 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class MarkdownParseTest {
 
     @Test                                      // forces this method to be run
@@ -7,6 +12,12 @@ public class MarkdownParseTest {
         assertEquals(2, 1 + 1);        /* Uses the assertEquals method (from import)
                                                    to see check if the arguments are equal then
                                                    prints the result */
+    }
+
+
+    @Test
+    public void testfile1() throws IOException {
+        assertEquals(List.of("https://something.com", "some-thing.html"), MarkdownParse.getLinks(Files.readString(Path.of("test-file.md"))));
     }
 
 }
